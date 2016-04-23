d27d5c2d8c9105099ca0c35775bf930000177c79
David Cabo <david.cabo@gmail.com>
2016-04-23 12:09:19 +0200

    Add language detection

--- javascript/listentoyourself.js
+++ javascript/listentoyourself.js
@@ -7,11 +7,23 @@ var button = form.find('.tweet-action');
 button.on("click", function(event) {
   console.log("Oh, listen to yourself...");
 
+  // Try to detect the text language
+  var inputText = "Listen to yourself...";
+  chrome.i18n.detectLanguage(inputText, function(result) {
+    var outputLang = "Detected Language: ";
+    var outputPercent = "Language Percentage: ";
+    for(i = 0; i < result.languages.length; i++) {
+      outputLang += result.languages[i].language + " ";
+      outputPercent +=result.languages[i].percentage + " ";
+    }
+    console.log(outputLang + "\n" + outputPercent + "\nReliable: " + result.isReliable);
+  });
+
   // Say something
   // var msg = new SpeechSynthesisUtterance('Oh, listen to yourself...');
   // msg.lang='en-US';
   var msg = new SpeechSynthesisUtterance('Oh, escúchate...');
-  msg.lang='es';
+  msg.lang='es';  // Seems to pick Diego
 
   window.speechSynthesis.speak(msg);
 
